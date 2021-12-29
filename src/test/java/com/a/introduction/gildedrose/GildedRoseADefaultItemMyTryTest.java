95e51842fb3067d6026a513971c9aeed3db20f07
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2021-12-29 23:42:11 +0530

    [Aparna] Refactor . Constant instead of values in GildedRoseADefaultItemMyTryTest.

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemMyTryTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemMyTryTest.java
@@ -8,6 +8,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseADefaultItemMyTryTest {
+
+	public static final int NOT_EXPIRED_SELLIN = 15;
+	public static final int INITIAL_QUALITY = 3;
+	public static final int NEGATIVE_SELL_IN = -1;
+	public static final String DEFAULT_ITEM = "DEFAULT_ITEM";
+
+
 	/**
 	 * Method to test the variation in quality of the item for the non expired
 	 * Item.
@@ -20,17 +27,20 @@ public class GildedRoseADefaultItemMyTryTest {
 	/* The Changes I made in this file
 	*    1. Making arrays to list
 	*    2. Making variables in item class private and adding getters to get it
+	*    3. Making constants.
 	* */
 	@Test
 	public void testUpdateQualityDefault1() {
-		Item item = new Item("DEFAULT_ITEM", 15, 3);
+		Item item = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELLIN, INITIAL_QUALITY); // Here we changed to constant because for better readability
 		List<Item> items = new ArrayList();
 		items.add(item);
 		GildedRose app = new GildedRose(items);
+
 		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items.get(0).getName());
-		assertEquals(14, app.items.get(0).getSellIn());
-		assertEquals(2, app.items.get(0).getQuality());
+
+		assertEquals(DEFAULT_ITEM, app.items.get(0).getName());
+		assertEquals(NOT_EXPIRED_SELLIN-1, app.items.get(0).getSellIn());
+		assertEquals(INITIAL_QUALITY-1, app.items.get(0).getQuality());
 	}
 
 	/**
@@ -42,13 +52,15 @@ public class GildedRoseADefaultItemMyTryTest {
 	 */
 	@Test
 	public void testUpdateQualityForExpiredItem() {
-		Item item = new Item("DEFAULT_ITEM", -1, 3);
+		Item item = new Item(DEFAULT_ITEM, NEGATIVE_SELL_IN, INITIAL_QUALITY);
 		List<Item> items = new ArrayList();
 		items.add(item);
 		GildedRose app = new GildedRose(items);
+
 		app.updateQuality();
+
 		assertEquals("DEFAULT_ITEM", app.items.get(0).getName());
-		assertEquals(-2, app.items.get(0).getSellIn());
-		assertEquals(1, app.items.get(0).getQuality());
+		assertEquals(NEGATIVE_SELL_IN-1, app.items.get(0).getSellIn());
+		assertEquals(INITIAL_QUALITY-2, app.items.get(0).getQuality());
 	}
 }
