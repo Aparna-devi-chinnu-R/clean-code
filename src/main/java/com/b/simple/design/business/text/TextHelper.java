e95c37f03aca7929b05cb977736eb272fcc176ab
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-01-09 21:05:40 +0530

    [Aparna] Add . Logic to swap last two characters of a string.

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,10 +3,16 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		if(str.length() < 2) return str;
+		char lastChar = str.charAt(str.length() - 1);
+		char secondLastChar = str.charAt(str.length() - 2);
+
+		String remainingString = str.substring(0, str.length() - 2);
+		return remainingString + lastChar +secondLastChar;
 	}
 
 	public String truncateAInFirst2Positions(String str) {
+
 		return null;
 	}
 }
